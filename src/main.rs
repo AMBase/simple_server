437c237dd64bc1a2e87ef99d9ef51d3819232027
AMBase <milnikovalexei@gmail.com>
2023-11-12 01:49:08 +0300

    feat: run TcpListener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(connection) => handle_connection(connection),
+            Err(e) => println!("{e}"),
+        }
+    }
+}
+
+fn handle_connection(tcp_stream: TcpStream) {
+    println!("{:?}", tcp_stream.local_addr())
 }
