9353382fd0f3851a6b017957a92184da4109c35b
AMBase <milnikovalexei@gmail.com>
2023-11-18 00:56:18 +0300

    feat: read http request by bytes from tcp

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,34 @@ use std::net::{TcpListener, TcpStream};
 use std::io::BufReader;
 use std::io::prelude::*;
 
+#[derive(Debug)]
+struct Error {
+    code: u16,
+    message: String,
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(connection) => handle_connection(connection),
-            Err(e) => println!("{e}"),
-        }
+        let connection = stream.unwrap();
+        let r = handle_connection(connection);
+        println!("{:?}", r.unwrap());
     }
+
+    ()
 }
 
-fn handle_connection(mut tcp_stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut tcp_stream);
-    let request: Vec<String> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+fn handle_connection(mut tcp_stream: TcpStream) -> Result<(), Error> {
+    let request = parse_request(&tcp_stream)?;
+    println!("{:?}", request);
 
-    parse_request(&request);
+    // let buf_reader = BufReader::new(&tcp_stream);
+    // let request: Vec<String> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
 
     let result = handle_request(request);
     let response = match result {
@@ -32,7 +39,7 @@ fn handle_connection(mut tcp_stream: TcpStream) {
             let data = format!("HTTP/1.1 {} {}\r\n", error.code, error.message).to_string();
             let result = tcp_stream.write_all(data.as_bytes());
             println!("{:#?}", result.unwrap());
-            return ();
+            return Ok(());
         }
     };
 
@@ -49,6 +56,8 @@ fn handle_connection(mut tcp_stream: TcpStream) {
 
     let result = tcp_stream.write_all(data.as_bytes());
     println!("{:#?}", result.unwrap());
+
+    Ok(())
 }
 
 struct Response {
@@ -57,18 +66,15 @@ struct Response {
     body: String,
 }
 
-struct Error {
-    code: u16,
-    message: String,
-}
 
 
-fn handle_request(request: Vec<String>) -> Result<Response, Error> {
+
+fn handle_request(request: Request) -> Result<Response, Error> {
     println!("{request:#?}");
 
-    if request.len() <= 0 {
-        return Err(Error {code: 500, message: String::from("Internal Server Error")});
-    }
+    // if request.len() <= 0 {
+    //     return Err(Error {code: 500, message: String::from("Internal Server Error")});
+    // }
 
     let status = 200;
     let headers = vec![
@@ -80,10 +86,28 @@ fn handle_request(request: Vec<String>) -> Result<Response, Error> {
 
     Ok(response)
 }
+#[derive(Debug)]
+struct Request {
 
+}
 
-fn parse_request(request: &Vec<String>) {
-    for line in request {
-        println!("{:?}", line.as_bytes());
+fn parse_request(tcp_stream: &TcpStream) -> Result<Request, Error> {
+    let request = Request {};
+    let mut reader = BufReader::new(tcp_stream);
+    let mut raw_request: Vec<u8> = Vec::new();
+    let mut buffer = [0u8; 1024];
+
+    loop {
+        let bytes_read = reader.read(&mut buffer).unwrap();
+        raw_request.extend_from_slice(&buffer[..bytes_read]);
+        if bytes_read < 1024 {
+            break;
+        }
     }
+
+
+
+    println!("{:?}", raw_request);
+
+    Ok(request)
 }
